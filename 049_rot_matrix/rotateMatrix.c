e0d6af27269299a0f0eea4701e2c44351500478b
yl730 <yl730@duke.edu>
2020-10-11 11:01:08 +0800

    1

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void rotate(FILE * f){
+void helper(FILE * f){
   int a;
-  int r =0;
-  int c = 0;
   char m[10][10]={0};
-  while ((c = fgetc(f)) != EOF) {
+  int r = 0;
+  int c = 0;
+ 
+  while ((a = fgetc(f)) != EOF) {
     if(a!='\n'){
       if(a < 0 || a >255){
-	fprintf(stderr, "error\n");
+	fprintf(stderr, "error a\n");
 	exit(EXIT_FAILURE);
       }
       if(r >= 10 || c >= 10){
-	fprintf(stderr, "error\n");
+	fprintf(stderr, "error c&r\n");
 	exit(EXIT_FAILURE);
+      } else{
+	m[r][c] = a;
+	c++;
       }
-      m[r][c] = a;
-      c++;
+      
     } else{
       if(c != 10){
-        fprintf(stderr, "error\n");
+        fprintf(stderr, "error c\n");
         exit(EXIT_FAILURE);
-      }
-      c = 0;
-      r++;
-      if(r > 10){
-	fprintf(stderr, "error\n");
-	exit(EXIT_FAILURE);
+      } else {
+	c = 0;
+	r++;
       }
     }
   }
 
-  if(c != 10 || r!= 10){
+  if(c != 0 || r!= 10){
     fprintf(stderr, "error\n");
     exit(EXIT_FAILURE);
   }
@@ -54,7 +54,7 @@ int main(int argc, char ** argv){
     perror("Could not open file");
     return EXIT_FAILURE;
   }
-  rotate(f);
+  helper(f);
   if (fclose(f) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
