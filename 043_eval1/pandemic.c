d123b1b4090ca5cd13c5b3ace0818a826ae2d976
yl730 <yl730@duke.edu>
2020-10-12 21:29:48 +0800

    1

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,25 +1,52 @@
 #include "pandemic.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 country_t parseLine(char * line) {
   //WRITE ME
   country_t ans;
-  for(int i = 0;i<strlen(line);i++){
+  size_t l = strlen(line);
+  size_t p = 0;
+  for(int i = 0;i<l;i++){
     if(line[i] != ','){
+      //check if the name is longer than 64
+      if(p >=64){
+	fprintf(stderr, "name is too long\n");
+	exit(EXIT_FAILURE);
+	//perror("name is too long");
+	//return EXIT_FAILURE;
+      }
       ans.name[i] = line[i];
+      p++;
+
+      //check if , didn't appear
+      if(i == l-1){
+	fprintf(stderr, "there's no population1\n");
+	exit(EXIT_FAILURE);
+	//perror("there's no population");
+	//return EXIT_FAILURE;
+      }
     } else {
+      //check if , appear at the end of the line
+      if(i==l-1){
+	fprintf(stderr, "there's no population2\n");
+	exit(EXIT_FAILURE);	
+	//perror("there's no population");
+      }
+      //end of name
       ans.name[i] = '\0';
-      if(i >= strlen(len -1)){
-        perror("something wrong");
-	return EXIT_FAILURE;
+      //change the remaining to int and put it into population
+      ans.population = atoi(line+i+1);
+      //if pop is unsigned
+      if(ans.population < 0){
+	fprintf(stderr, "type of pop is wrong\n");
+	exit(EXIT_FAILURE);
+	//perror("type of pop is wrong");	
       }
-      for(int j = i+1; j < strlen(line);j++){
-        
-      }
+      return ans;
     }
   }
-  
-  
   //ans.name[0] = '\0';
   //ans.population = 0;
   return ans;
