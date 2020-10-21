8cf18ff715f939985edd176cf01db5b93fa8ef1e
yl730 <yl730@duke.edu>
2020-10-21 21:08:25 +0800

    1

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -4,14 +4,49 @@
 #include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * c = malloc(sizeof(*c));
+  c->arr = NULL;
+  c->sz = 0;
+  c->unknown = 0;
+  return c;
 }
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if(name == NULL){
+    c->unknown++;
+  } else {
+    int new = 1;
+    for(size_t i = 0; i<c->sz;i++){
+      if(strcmp(c->arr[i].c,name)==0){
+	new = 0;
+	c->arr[i].len++;
+	break;
+      }
+    }
+    if(new == 1){
+      c->arr = realloc(c->arr,(c->sz+1)*sizeof(*c->arr));
+      c->arr[c->sz].c = strdup(name);
+      c->arr[c->sz].len = 1;
+      c->sz++;
+    }
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  for(size_t i = 0;i < c->sz;i++){
+    fprintf(outFile,"%s: %d\n",c->arr[i].c,c->arr[i].len);
+  }
+  if(c->unknown > 0){
+    fprintf(outFile,"<unknown>: %d\n",c->unknown);
+  }
+  
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for(size_t i = 0;i < c->sz;i++){
+    free(c->arr[i].c);
+  }
+  free(c->arr);
+  free(c); 
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -2,12 +2,17 @@
 #define __COUNTS_H__
 struct _one_count_t {
   //DEFINE ME
-
+  char * c;
+  int len;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t * arr;
+  size_t sz;
+  int unknown;
+
 };
 typedef struct _counts_t counts_t;
 
