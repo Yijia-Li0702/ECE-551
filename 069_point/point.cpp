cba35b2fe9c57f331de99b89e880bf9fd8d8737e
yl730 <yl730@duke.edu>
2020-10-28 23:14:56 +0800

    1

---
+++ 069_point/point.cpp
@@ -0,0 +1,16 @@
+#include "point.hpp"
+
+#include <cmath>
+#include <cstdio>
+
+void Point::move(double dx, double dy){
+  x = x + dx;
+  y = y + dy;
+}
+double Point::distanceFrom(const Point & p){
+  double res;
+  double x_dis = p.x - x;
+  double y_dis = p.y - y;
+  res = std::sqrt(x_dis*x_dis + y_dis*y_dis);
+  return res;
+}
