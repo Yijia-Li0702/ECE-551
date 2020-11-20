749ecc673834144017e8befa9a5d084448b3ae70
yl730 <yl730@duke.edu>
2020-11-20 20:55:12 +0800

    !

--- 097_binsrch/search.cpp
+++ 097_binsrch/search.cpp
@@ -3,16 +3,12 @@
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
-  while (low + 1 < high) {
-  if(low == high || low == high - 1){
+    if(low == high || low == high - 1){
      return low;
    }
-   if (f->invoke(low) >= 0) {
-    return low;
-  }
-  if (f->invoke(high - 1) <= 0) {
-    return high - 1;
-  }
+  
+  while (low + 1 < high) {
+
     int mid = (low + high) / 2;
     int val = f->invoke(mid);
     if (val == 0) {
