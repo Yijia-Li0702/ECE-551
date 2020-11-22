418318390b4ba3cd2c9c92afb43315dc915a35a4
yl730 <yl730@duke.edu>
2020-11-22 22:52:59 +0800

    1

--- 098_bst_map/bstmap.h
+++ 098_bst_map/bstmap.h
@@ -20,18 +20,20 @@ class BstMap : public Map<K, V> {
  public:
   BstMap() : root(NULL) {}
    virtual void add(const K & key, const V & value) {
-    Node ** ptr = &root;
-    while((*ptr)!=NULL){
-       if((*ptr)->key>key){
-        ptr = &(*ptr)->left;
-      }else if((*ptr)->key<key){
-        ptr = &(*ptr)->right;
-      }else if((*ptr)->key == key){
-        (*ptr)->value = value;
+    Node ** curr = &root;
+    while (*curr != NULL) {
+      if (key == (*curr)->key) {
+        (*curr)->value = value;
         return;
       }
+      if (key < (*curr)->key) {
+        curr = &(*curr)->left;
+      }
+      else {
+        curr = &(*curr)->right;
+      }
     }
-    *ptr = new Node(key,value);
+    *curr = new Node(key, value);
   }
   virtual const V & lookup(const K& key) const throw (std::invalid_argument) {
      Node * ptr = root;
