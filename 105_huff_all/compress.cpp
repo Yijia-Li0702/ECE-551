772737fe3896da23908969207a87e99ae8fc2fd9
yl730 <yl730@duke.edu>
2020-11-27 14:12:55 +0800

    1

--- 105_huff_all/compress.cpp
+++ 105_huff_all/compress.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
 #include <stdint.h>
 #include <map>
 #include <queue>
@@ -32,12 +33,26 @@ void writeCompressedOutput(const char* inFile,
 
   //WRITE YOUR CODE HERE!
   //open the input file for reading
-
+  std::ifstream ifs;
+  ifs.open(inFile, std::ifstream::in);
+  int c;
+  while((c=ifs.get())!=EOF){
+    //std::map<unsigned,BitString>::iterator it = theMap.find(c);
+   // if(it!=theMap.end()){
+      bfw.writeBitString(theMap.find(c)->second);
+    //}
+    
+  }
   //You need to read the input file, lookup the characters in the map,
   //and write the proper bit string with the BitFileWriter
 
   //dont forget to lookup 256 for the EOF marker, and write it out.
-
+    //std::map<unsigned,BitString>::iterator it = theMap.find(c);
+   // if(it!=theMap.end()){
+      bfw.writeBitString(theMap.find(256)->second);
+    //}
+    
+    ifs.close();
   //BitFileWriter will close the output file in its destructor
   //but you probably need to close your input file.
 }
@@ -51,7 +66,14 @@ int main(int argc, char ** argv) {
   //Implement main
   //hint 1: most of the work is already done. 
   //hint 2: you can look at the main from the previous tester for 90% of this
-
-
+  uint64_t * freq = readFrequencies(argv[1]);
+  Node * root= buildTree(freq);
+  BitString b;
+  std::map<unsigned,BitString> theMap;
+  root->buildMap(b,theMap);
+  writeCompressedOutput(argv[1], argv[2], theMap);
+  delete[] freq;
+  delete root;
   return EXIT_SUCCESS;
+
 }
