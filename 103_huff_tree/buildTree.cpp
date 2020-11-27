1f76e061ea416697bf347efc2c2144dc22a6f435
yl730 <yl730@duke.edu>
2020-11-27 12:32:44 +0800

    1

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -10,7 +10,7 @@ Node *  buildTree(uint64_t * counts) {
   //WRITE ME!
 	priority_queue_t pd;
   for(size_t i=0;i< 257;i++){
-  if(counts[i ]!=0){
+  if(counts[i]!=0){
   	pd.push(new Node(i,counts[i]));
    }
   }
@@ -19,12 +19,6 @@ Node *  buildTree(uint64_t * counts) {
    pd.pop();
    Node * right = pd.top();
    pd.pop();
-//   if(pd.size()==1){
-//   Node * n =new Node(left,right);
-//   return n;
-//   }
- 	
- 	//uint64_t n-freq = left.n-freq+right.n-freq;
  	Node * n =  new Node(left,right);
  	pd.push(n);
   
--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -3,11 +3,13 @@
 void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
   //WRITE ME!
   if(sym == NO_SYM){
+    assert(left != NULL&&right != NULL);
     this->right->buildMap(b.plusOne(), theMap);
     //BitString l = b.
     //this->right->buildMap(b.insertByte('1'), theMap);
     this->left->buildMap(b.plusZero(), theMap);
   } else{
+  assert(left == NULL&&right == NULL);
     theMap.insert(std::pair<unsigned,BitString>(sym,b));
   }
   return;
